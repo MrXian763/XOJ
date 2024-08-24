89d175535649a57258577ca845ee9031c4094f96
xianzicai <xianzhiyong763@gmail.com>
2024-08-24 16:08:49 +0800

    阿里云脚手架创建微服务初始项目

---
+++ src/main/java/com/zicai/xojbackendmicroservice/XojBackendMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.zicai.xojbackendmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XojBackendMicroserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XojBackendMicroserviceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zicai/xojbackendmicroservice/XojBackendMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zicai.xojbackendmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XojBackendMicroserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
