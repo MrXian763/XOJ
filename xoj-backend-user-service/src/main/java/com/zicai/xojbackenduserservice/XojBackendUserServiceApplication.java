2b00f34a51478b18a95f683ff23d8d1d9cb153cb
xianzicai <xianzhiyong763@gmail.com>
2024-08-24 19:22:40 +0800

    用户服务搬运

--- xoj-backend-user-service/src/main/java/com/zicai/xojbackenduserservice/XojBackendUserServiceApplication.java
+++ xoj-backend-user-service/src/main/java/com/zicai/xojbackenduserservice/XojBackendUserServiceApplication.java
@@ -1,9 +1,17 @@
 package com.zicai.xojbackenduserservice;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@MapperScan("com.zicai.xojbackenduserservice.mapper")
+@EnableScheduling
+@EnableAspectJAutoProxy(proxyTargetClass = true, exposeProxy = true)
+@ComponentScan("com.zicai") // 加载到不同服务下的 bean
 public class XojBackendUserServiceApplication {
 
     public static void main(String[] args) {
