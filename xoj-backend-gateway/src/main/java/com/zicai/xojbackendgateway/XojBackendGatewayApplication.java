517d375f803eddc871ec6b11a63cc0ab30204e52
xianzicai <xianzhiyong763@gmail.com>
2024-08-25 11:41:41 +0800

    配置 OpenFeign 服务注册与发现

--- xoj-backend-gateway/src/main/java/com/zicai/xojbackendgateway/XojBackendGatewayApplication.java
+++ xoj-backend-gateway/src/main/java/com/zicai/xojbackendgateway/XojBackendGatewayApplication.java
@@ -2,8 +2,10 @@ package com.zicai.xojbackendgateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient // Nacos 注册服务
 public class XojBackendGatewayApplication {
 
     public static void main(String[] args) {
--- xoj-backend-user-service/src/main/java/com/zicai/xojbackenduserservice/XojBackendUserServiceApplication.java
+++ xoj-backend-user-service/src/main/java/com/zicai/xojbackenduserservice/XojBackendUserServiceApplication.java
@@ -3,6 +3,8 @@ package com.zicai.xojbackenduserservice;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -12,6 +14,8 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling
 @EnableAspectJAutoProxy(proxyTargetClass = true, exposeProxy = true)
 @ComponentScan("com.zicai") // 加载到不同服务下的 bean
+@EnableDiscoveryClient // Nacos 注册服务
+@EnableFeignClients(basePackages = {"com.zicai.xojbackendserviceclient.service"}) // Feign 调用服务
 public class XojBackendUserServiceApplication {
 
     public static void main(String[] args) {
