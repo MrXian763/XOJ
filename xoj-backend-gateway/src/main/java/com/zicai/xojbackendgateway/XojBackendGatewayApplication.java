acf50e2a6d7da02c7c121060f8d668f4b8e6849a
xianzicai <xianzhiyong763@gmail.com>
2024-08-26 11:33:26 +0800

    Gateway 网关路由配置

--- xoj-backend-gateway/src/main/java/com/zicai/xojbackendgateway/XojBackendGatewayApplication.java
+++ xoj-backend-gateway/src/main/java/com/zicai/xojbackendgateway/XojBackendGatewayApplication.java
@@ -2,9 +2,10 @@ package com.zicai.xojbackendgateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 @EnableDiscoveryClient // Nacos 注册服务
 public class XojBackendGatewayApplication {
 
